755b69dff083b3da0a790093ce2c9b02c0d38fb0
Suraj Shah <surajshah.bombay@gmail.com>
2022-11-30 21:20:31 +0530

    Added first page

--- screens/LogIn.js
+++ screens/LogIn.js
@@ -2,42 +2,29 @@ import React from 'react'
 import { Text, View, StyleSheet } from 'react-native'
 import BlueCircle from '../assets/pictures/BlueCircle.svg';
 import { BlurView } from '@react-native-community/blur';
+import { Svg, Circle,  } from 'react-native-svg';
+import BlockTubeLogo from '../assets/pictures/BlockTubeLogo.svg'
 
 
 const LogIn = () => {
   return (
     <>
-    
-       
-    {/* <View style = {styles.bg} /> */}
-    <View style={styles.bg}></View>
-    <BlueCircle style={styles.blueCircle}/>
-    
-    <BlurView blurType="light"
-       blurAmount={50}
-       blurRadius={10}
-       style={{height: "100%", width: "100%"}}
-       reducedTransparencyFallbackColor="white"/>
-    
-       
-    <BlurView blurType="light"
-       blurAmount={50}
-       blurRadius={10}
-       style={{height: "100%", width: "100%"}}
-       reducedTransparencyFallbackColor="white"/>
-       
-    <BlurView blurType="light"
-       blurAmount={50}
-       blurRadius={10}
-       style={{height: "100%", width: "100%"}}
-       reducedTransparencyFallbackColor="black"/>
-       <View style={styles.topView}></View>
-       
-       
-       
-     
-     
-       </>
+      <View style = {styles.bg}>
+        <View>
+          <Text style={styles.welcome}>Welcome to</Text>
+          <Text style={styles.BT}>BlockTube</Text>
+        </View>
+        <View style={styles.logo}>
+          <BlockTubeLogo style={styles.image}/>
+          <View style={styles.wctext}>
+            <Text style={styles.subtitle}>Lorem Ipsum Dolor</Text>
+            <Text style={styles.description}>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </Text>
+          </View>
+        </View>
+      </View>
+
+
+    </>
   )
 }
 
@@ -45,19 +32,49 @@ const styles = StyleSheet.create({
   bg: {
     backgroundColor: "black",
     width: "100%",
-    height: "100%"
+    height: "100%",
+    padding: 20,
   },
-  topView:{
+
+  welcome: {
     color: "white",
-    width: 100,
-    height: 100
+    fontFamily: "Kanit-ExtraLight",
+    fontSize: 36,
+  
   },
-  blueCircle: {
-    position: 'absolute',
-     top: 0,
-     left: 0,
-     bottom: 0,
+
+  BT: {
+    color: "white",
+    fontFamily: "Kanit-Bold",
+    fontSize: 48,
+  },
+
+  logo: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: "80%",
+    textAlign: "right"
+  },
+
+
+  subtitle: {
+    color: "white",
+    fontFamily: "Kanit-ExtraLight",
+    fontSize: 24,  
+    marginTop: "25%"  
+  },
+
+  description: {
+    color: "white",
+    fontFamily: "Kanit-ExtraLight",
+    fontSize: 14,
+    marginTop: "5%"
+  },
+
+  wctext: {
+    alignItems: "flex-start",
   }
+  
 })
 
 export default LogIn
